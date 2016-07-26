89c76e82b66c936c1765c253dd5cb60738d87bc8
Tom Bevan <tom.bevan@r3labs.io>
2016-07-26 11:55:35 +0100

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Waiting for NATS"
-while ! echo exit | nc postgres 4222; do sleep 1; done
+while ! echo exit | nc nats 4222; do sleep 1; done
 
 echo "Starting api-gateway"
 /go/bin/api-gateway
