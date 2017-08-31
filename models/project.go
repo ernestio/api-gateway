24adefe609649f995b76dd9f7be4e19df923b0b9
Tom Bevan <tom.bevan@r3labs.io>
2017-08-31 17:52:25 +0100

    fixing redaction of username for vcloud

--- models/project.go
+++ models/project.go
@@ -118,7 +118,7 @@ func (d *Project) Delete() (err error) {
 // data before outputting to the user
 func (d *Project) Redact() {
 	for k, v := range d.Credentials {
-		if k == "region" || k == "external_network" || k == "org" {
+		if k == "region" || k == "external_network" || k == "username" {
 			sv, ok := v.(string)
 			if !ok {
 				log.Println("could not assert credential value")
