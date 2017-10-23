214591e749fcdccc49186de9c30224a4085626ac
Mark Newman <mpnewmanz@gmail.com>
2017-10-23 12:02:31 +0100

    Update license check

--- helpers/licensed.go
+++ helpers/licensed.go
@@ -7,7 +7,7 @@ import (
 
 // Licensed : Checks if the current api is running with premium support
 func Licensed() error {
-	if len(os.Getenv("ERNEST_PREMIUM")) == 0 {
+	if os.Getenv("ERNEST_EDITION") != "enterprise" {
 		return errors.New("You're running ernest community edition, please contact R3Labs for premium support")
 	}
 	return nil
