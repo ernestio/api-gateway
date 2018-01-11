8326543faaa65e200ee0d45396048eea8b4030c1
Mark Newman <mark.newman@r3labs.io>
2018-01-11 13:50:30 +0000

    Fix environments update

--- controllers/policies/update.go
+++ controllers/policies/update.go
@@ -29,6 +29,10 @@ func Update(au models.User, name string, body []byte) (int, []byte) {
 	existing.Definition = d.Definition
 	existing.Environments = d.Environments
 
+	if len(existing.Environments) == 0 {
+		existing.Environments = make([]string, 0)
+	}
+
 	if err = existing.Save(); err != nil {
 		h.L.Error(err.Error())
 		return 500, []byte("Internal server error")
