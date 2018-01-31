8a1bb645a2ed6d082d5845af6a43f9210aecc1d5
Mark Newman <mark.newman@r3labs.io>
2018-01-31 13:51:46 +0000

    Update readme

--- helpers/licensed.go
+++ helpers/licensed.go
@@ -8,7 +8,7 @@ import (
 // Licensed : Checks if the current api is running with premium support
 func Licensed() error {
 	if os.Getenv("ERNEST_EDITION") != "enterprise" {
-		return errors.New("You're running ernest community edition, please contact R3Labs for premium support")
+		return errors.New("You're running ernest community edition, please visit http://docs.ernest.io/support for premium support")
 	}
 	return nil
 }
