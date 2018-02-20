46355e2bfdc272976e4e0525a5af539fb630f7f0
Tom Bevan <tom.bevan@r3labs.io>
2018-02-20 17:32:01 +0000

    updated model to include more detail on compliance output

--- models/validation.go
+++ models/validation.go
@@ -10,16 +10,52 @@ type BuildValidate struct {
 type BuildValidateResponse struct {
 	Version    string     `json:"version"`
 	Controls   []Control  `json:"controls"`
+	Profiles   []Profile  `json:"profiles"`
 	Statistics Statistics `json:"statistics"`
+}
+
+// Profile describes the policy document and its test results
+type Profile struct {
+	Supports []string         `json:"supports"`
+	Title    string           `json:"title"`
+	Name     string           `json:"name"`
+	Controls []ControlDetails `json:"controls"`
+}
+
+// ControlDetails describes additional information about a control
+type ControlDetails struct {
+	ID             string    `json:"id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"desc"`
+	Impact         string    `json:"impact"`
+	References     []string  `json:"refs"`
+	Tags           []string  `json:"tags"`
+	Code           string    `json:"code"`
+	Results        []Control `json:"results"`
+	Groups         []Group   `json:"groups"`
+	Attributes     []string  `json:"attribues"`
+	SHA256         string    `json:"sha256"`
+	SourceLocation struct {
+		Reference string `json:"ref"`
+		Line      string `json:"line"`
+	} `json:"source_location"`
 }
 
 // Control describes an individual test within a build validation.
 type Control struct {
-	ID        string `json:"id"`
-	ProfileID string `json:"profile_id"`
-	Status    string `json:"status"`
-	CodeDesc  string `json:"code_desc"`
-	Message   string `json:"message"`
+	ID        string  `json:"id"`
+	ProfileID string  `json:"profile_id"`
+	Status    string  `json:"status"`
+	CodeDesc  string  `json:"code_desc"`
+	RunTime   string  `json:"run_time"`
+	StartTime float64 `json:"start_time"`
+	Message   string  `json:"message"`
+}
+
+type Group struct {
+	ID       string   `json:"id"`
+	Title    string   `json:"title"`
+	Controls []string `json:"controls"`
 }
 
 // Statistics describes stats for the build validate service.
