53af5dbfc4a42f967aadbd2856710b281a709d85
Mark Newman <mark.newman@r3labs.io>
2018-06-21 15:08:13 +0100

    Fix project membership so there is always at least one owner

--- controllers/roles/delete_by_id.go
+++ controllers/roles/delete_by_id.go
@@ -16,6 +16,26 @@ func DeleteByID(au models.User, id string) (int, []byte) {
 		return 404, models.NewJSONError("Not found")
 	}
 
+	if existing.ResourceType == "project" {
+		var roles []models.Role
+		var owner bool
+
+		err := existing.FindAllByResource(existing.ResourceID, existing.ResourceType, &roles)
+		if err != nil {
+			return 500, models.NewJSONError(err.Error())
+		}
+
+		for _, v := range roles {
+			if v.Role == "owner" && v.UserID != existing.UserID {
+				owner = true
+			}
+		}
+
+		if !owner {
+			return 400, models.NewJSONError("Cannot remove the only project owner")
+		}
+	}
+
 	if err := existing.Delete(); err != nil {
 		return 500, models.NewJSONError("Internal server error")
 	}
